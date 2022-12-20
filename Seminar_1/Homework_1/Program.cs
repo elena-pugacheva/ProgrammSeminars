a3bd4eb5deb140ad1947a9c095d29ac23db7d849
ElenaPugacheva <elenapugacheva2708@gmail.com>
2022-12-20 12:28:55 +0300

    homework seminar1

---
+++ Seminar_1/Homework_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int number1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2=Convert.ToInt32(Console.ReadLine());
+
+if (number1>number2)
+{
+    Console.Write($"max={number1}");
+}
+else
+{
+    Console.Write($"max={number2}");
+}
+
+if (number1==number2)
+{
+    Console.Write("числа равны!");
+}
---
+++ Seminar_1/Homework_4/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите положительное число выше 1: ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int i=1;
+
+Console.WriteLine($"Чётные числа от 1 до {number}: ");
+
+while (i <= number)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}, ");
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
+
+
