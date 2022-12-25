2d5d674835f20fad4eab45717f5b82d1390989da
ElenaPugacheva <elenapugacheva2708@gmail.com>
2022-12-26 00:35:33 +0300

    HW for Seminar3

---
+++ Seminar_3/Homework_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+ if (number<0)
+    {
+        number=number*(-1);
+    }
+    
+if (number>9999 & number<100000)
+{
+    if (number/10000==number%10 && (number/1000)%10==(number%100)/10)
+    {
+        Console.Write("Палиндром.");
+    }
+    else
+    {
+        Console.Write("Не палиндром.");
+    }
+}
+else
+{
+    Console.Write("Неверное число");
+}
+
---
+++ Seminar_3/Homework_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите X1: ");
+int X1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y1: ");
+int Y1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Z1: ");
+int Z1=Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите X2: ");
+int X2=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y2: ");
+int Y2=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Z2: ");
+int Z2=Convert.ToInt32(Console.ReadLine());
+
+int A = X2 - X1;
+int B = Y2 - Y1;
+int C = Z2 - Z1;
+
+double res=Math.Sqrt(Math.Pow(A,2)+Math.Pow(B,2)+Math.Pow(C,2));
+Console.WriteLine($"Расстояние между точками={res}");
