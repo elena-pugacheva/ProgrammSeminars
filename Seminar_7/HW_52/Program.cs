ed7e346912e72094ae604562dd8318c34c66b423
ElenaPugacheva <elenapugacheva2708@gmail.com>
2023-01-25 14:42:54 +0300

    добавила дз 7 семинара

---
+++ Seminar_7/HW_52/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)   //задали массив
+{
+    int[,] matr = new int[rows, columns];
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)         //напечатали массив
+{
+    for (int i = 0; i < matr.GetLength(0); i++)        //o означает кол-во строк
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)   //1 означает кол-во столбцов
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int rows = 3;
+const int columns = 4;                      
+const int leftRange = 0;
+const int rightRange = 9;
+
+int[,] array = GetRandomMatrix(rows, columns, leftRange, rightRange);
+PrintMatrix(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    double mean = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = sum + array[i, j];
+    }
+    mean = sum / array.GetLength(0);
+    Console.Write($"{mean:f1}; ");
+}
+
